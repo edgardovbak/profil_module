d629069bedf57ac43415444b9852cb21f82ebcb4
dovbake <edhardowbak@gmail.com>
2018-06-12 13:31:40 +0200

    Update login

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -128,6 +128,7 @@ export class Login extends React.Component<Props, State> {
                             className="sn_btn"
                             id="submitLoginForm"
                             disabled={this.state.passwordValid && this.state.emailValid ? false : true}
+                            type="button"
                         >
                             login
                         </button>
