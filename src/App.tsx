90050637b1dd0446ed5b64478c0ff4f8b1aacc48
dovbake <edhardowbak@gmail.com>
2018-06-14 10:02:00 +0200

    update url

--- src/App.tsx
+++ src/App.tsx
@@ -41,7 +41,7 @@ class App extends React.Component<AppProps, any> {
             <Switch>
                 <Route 
                     exact={true}  
-                    path="/login:email?"   
+                    path="/login"   
                     render={routerProps => { 
                         return status ?
                                 <Login formSubmit={this.formSubmitHandler} />
